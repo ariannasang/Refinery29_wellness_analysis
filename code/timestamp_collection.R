5c4c5104a34dcedc7fea200a6bc641d814e9b31c
Arianna Sang <ariannasang@Ariannas-MacBook-Pro.local>
2020-05-29 20:02:21 -0400

    Include timestamp collection

---
+++ code/timestamp_collection.R
@@ -0,0 +1,22 @@
+# Collect the timestamps
+library(rvest)
+library(dplyr)
+library(lubridate)
+
+# Merge files to create complete df
+date_df <- read.csv('data/published_date.csv') %>% 
+  select(-X)
+incomplete_df <- readRDS('refinery29_cols.rda') 
+df <- merge(date_df, incomplete_df, by = c('Titles', 'Links'))
+
+# Correct date
+df$Pub_date[7] <- '5/26/20'
+df$Pub_date[31] <- '5/5/20'
+df$Pub_date <- df$Pub_date %>% as.Date(format = '%m/%d/%y') 
+
+saveRDS(df, 'data/refinery29_cols_time.rda')
+
+
+
+
+
